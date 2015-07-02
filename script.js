c0711d7e276a22733ea58186fd52c10107915567
GROUP 4 <SSSE2015@E6440_LEGO9.pace.edu>
2015-07-01 21:27:34 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+
+
+	var isShown = true;
+
+	var score = 0;
+
+	var clickFunction = function() {
+		score++;
+		isShown = !isShown;
+		if (isShown) {
+			$(this).show()
+		} else {
+			$(this).hide();
+		}
+		console.log(score)
+	}
+
+	var oysterClick = function() {
+		if(score > 0)
+			score--;
+		isShown = !isShown;
+		if (isShown) {
+			$(this).show()
+		} else {
+			$(this).hide();
+		}
+		console.log(score)
+	}
+
+	$("#garbage1").click(clickFunction);
+	$("#oyster").click(oysterClick);
+
+
+})
